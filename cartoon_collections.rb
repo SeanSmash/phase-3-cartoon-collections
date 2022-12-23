afcb25463ecc64717b2d88d647a94d0342aec0c3
Sean Sarokas <sps4462@gmail.com>
2022-12-23 00:03:53 -0500

    commit tree 7867e64166235a502225cc6599588a9facce80cb

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(array)
+  array.each_with_index do |item, index|
+    puts "#{index + 1}. #{item}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  shouts = calls.map do |c| 
+    "#{c.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  long_calls = calls.filter {|c| c.length > 4}
+  long_calls.any?
+      #binding.pry
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |item|
+    cheese_types.include?(item) ? item : nil
+  end
 end
